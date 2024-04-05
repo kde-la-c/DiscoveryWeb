f9ea811b6ccd5b79087bf84ee321a9d9d941c3f9
Kevin De la colina <kede-la-@c1r11s1.42madrid.com>
2024-04-05 16:54:50 +0200

    ex3.0 and 3.1 finished

--- cell03/ex00/script.js
+++ cell03/ex00/script.js
@@ -1,5 +1,4 @@
 function changeBackground() {
 	var randomColor = Math.floor(Math.random()*16777215).toString(16);
 	document.body.style.backgroundColor = "#" + randomColor;
-	console.log("HOLA");
 }
